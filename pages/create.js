d86a2c87af9b3b6d96cf49f49a4f65dcff8432f4
WendyWang68 <94095789+WendyWang68@users.noreply.github.com>
2023-11-28 21:55:22 -0500

    first commit

---
+++ pages/create.js
@@ -0,0 +1,12 @@
+import Header from "@/app/components/Header";
+
+export default function CreaterUser() {
+    return (
+        <>
+        <Header />
+            <main>
+                <h1>Create User</h1>
+            </main>
+        </>
+    );
+};
